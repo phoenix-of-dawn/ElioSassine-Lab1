f3eda92fe230c151f3a940aa53deca9336014721
Elio-Benoît Sassine <2493512@edu.vaniercollege.qc.ca>
2025-08-27 10:30:30 -0400

    Implemented GetBarCode function

--- src/ziptestdemo/ZipCode.java
+++ src/ziptestdemo/ZipCode.java
@@ -9,7 +9,52 @@ package ziptestdemo;
  * @author Elio Sassine - 2493512
  */
 public class ZipCode {
-    public ZipCode() {
+    public int Zip;
+    
+    // This is reversed because the GetBarCode function reverses it back
+    private static final String[] reversedZipToBarcodeMap = {
+        "00011", 
+        "11000", 
+        "10100", 
+        "01100", 
+        "10010", 
+        "01010", 
+        "00110",
+        "10001",
+        "01001",
+        "00101"
+    };
+            
+    public ZipCode(int zipCode) {
+        if (zipCode > 99999 || zipCode < 0) {
+            System.out.println("Error: Invalid zip code");
+            return;
+        }
         
+        this.Zip = zipCode;
+    }
+    
+    public ZipCode(String barCode) {
+        if (barCode.length() != 27) {
+            System.out.println("Error: Invalid barcode string");
+            return;
+        }
+        
+        // TODO: Convert to int
+    }
+    
+    public String GetBarCode() {
+        String reverseBarCode = "1";
+        for (int tempZip = this.Zip; tempZip > 0; tempZip /= 10) {
+            int currentDigit = tempZip % 10;
+            reverseBarCode += reversedZipToBarcodeMap[currentDigit];
+        }
+        reverseBarCode += "1";
+        
+        String barCode = "";
+        for (int i = barCode.length() - 1; i >= 0; i--) {
+            barCode += reverseBarCode.charAt(i);
+        }
+        return barCode;
     }
 }
